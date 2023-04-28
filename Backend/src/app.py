e827a29535b81e2398d542eb0c795550dbfb927c
Orelmis Toribio <ot53@cornell.edu>
2023-04-28 00:09:33 -0400

    Authentication Routes
    - Partially tested

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -4,6 +4,7 @@ import json
 from db import User
 import os
 import user_auth
+import datetime
 
 app = Flask(__name__)
 db_filename = "cms.db"
@@ -22,6 +23,19 @@ def success_response(data, code = 200):
 
 def fail_response(message, code = 404):
     return json.dumps({"error": message}), code
+
+
+def extract_token_from_header(request):
+    auth_header = request.headers.get("Authorization")
+    if auth_header is None:
+        return False, "Missing auth_header."
+    bearer_token = auth_header.replace("Bearer", "").strip()
+    if not bearer_token:
+        return False, "Invalid auth header"
+    return True, bearer_token
+    
+
+
 
 
 #ROUTES
@@ -49,8 +63,56 @@ def register_user():
         "update_token": user.update_token
     })
 
+@app.route("/login/", methods = ["POST"])
+def login():
 
+    body = json.loads(request.data)
+    
+    net_id = body.get("net_id")
+    password = body.get("password")
+    if net_id is None or password is None:
+        return fail_response("Invalid netID or passsword.")
+    success, user = user_auth.verify_credentials(net_id, password)
 
+    if not success:
+        return fail_response("Incorrect username or password.")
+    
+    return success_response({
+        "session_token": user.session_token,
+        "session_expiration": str(user.session_expiration),
+        "update_token": user.update_token 
+    })
+
+@app.route("/session/", methods = ["POST"])
+def update_session():
+    success, update_token = extract_token_from_header(request)
+
+    if not success:
+        return fail_response(update_token)
+    
+    user = user_auth.renew_session(update_token)
+
+    if user is None:
+        return fail_response("Invalid update token.")
+    return success_response(
+        {
+        "session_token": user.session_token,
+        "session_expiration": str(user.session_expiration),
+        "update_token": user.update_token
+        }
+    )
+
+@app.route("/logout/", methods = ["POST"])
+def logout():
+    success, session_token = extract_token_from_header(request)
+    if not success:
+        return fail_response(session_token)
+    user = user_auth.get_user_by_session_token(session_token)
+    if not user or not user.verify_session_token(session_token):
+        return fail_response("Invalid sessopmn token.")
+    user.session_expiration = datetime.datetime.now()
+    db.session.commit()
+    return success_response("Logged out successfully.")
 
 
 
